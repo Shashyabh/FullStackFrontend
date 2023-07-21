df64abdd2c1ae7c00eb916c01dbbd0688d581a80
shashyabh <shashyabhray@gmail.com>
2023-07-21 17:46:37 +0530

    Base Url added

--- src/services/Helper_Service.js
+++ src/services/Helper_Service.js
@@ -1,30 +1,27 @@
-export const BASE_URL=`http://localhost:7070`;
+// export const BASE_URL=`http://localhost:7070`;
 
-export const getProductImageUrl=(productId)=>{
-    return `${BASE_URL}/products/image/${productId}`;
-}
+export const BASE_URL = `shashyabhray.up.railway.app`;
+export const getProductImageUrl = (productId) => {
+	return `${BASE_URL}/products/image/${productId}`;
+};
 
-export const formateDate=(time)=>{
-    // var options={
-    //     weekday:"long",
-    //     year:"numeric",
-    //     month:"long",
-    //     day:"numeric"
-    // }
-    // return new Date(time).toLocaleDateString("hi-IN",Option);
-    // return new Date(time).toLocaleDateString("en-US",options);
+export const formateDate = (time) => {
+	// var options={
+	//     weekday:"long",
+	//     year:"numeric",
+	//     month:"long",
+	//     day:"numeric"
+	// }
+	// return new Date(time).toLocaleDateString("hi-IN",Option);
+	// return new Date(time).toLocaleDateString("en-US",options);
 
-    if(!time){
-        return null
-    }
-    const date=new Date(time);
-    return date.toLocaleString();
-}
+	if (!time) {
+		return null;
+	}
+	const date = new Date(time);
+	return date.toLocaleString();
+};
 
-
-export const getUserImageUrl=(userId)=>{
-
-    return `${BASE_URL}/users/image/${userId}`
-}
-
-
+export const getUserImageUrl = (userId) => {
+	return `${BASE_URL}/users/image/${userId}`;
+};
